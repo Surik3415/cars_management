35607909d6d4aa26945db81c53de463a76414b1d
SurenAvakian <Surenavakian96@gmail.com>
2022-11-14 00:06:32 +0200

    rename objects by proper way

--- index.rb
+++ index.rb
@@ -6,79 +6,79 @@ require 'yaml'
 require 'date' 
 
 # create a function to display the result on the screen
-def showRequestedCar 
-    $arrWithCars.each do |car|
+def show_requested_car 
+    $arr_with_cars.each do |car|
         car.each { |key, value| puts "#{key}: #{value}" }
         puts "---------------------------------------"
     end
 end
 
 
-$hashWithMyImputs = {}
+$hash_with_my_imputs = {}
 
 # get argument for search request
 puts "Please choose make: "
-$hashWithMyImputs[:make] = gets.capitalize.chomp
+$hash_with_my_imputs[:make] = gets.capitalize.chomp
 
 puts "Please choose model: "
-$hashWithMyImputs [:model] = gets.capitalize.chomp
+$hash_with_my_imputs [:model] = gets.capitalize.chomp
 
 puts "Please choose year_from: "
-$hashWithMyImputs[:year_from] = gets.chomp
+$hash_with_my_imputs[:year_from] = gets.chomp
 
 puts "Please choose year_to: "
-$hashWithMyImputs[:year_to] = gets.chomp
+$hash_with_my_imputs[:year_to] = gets.chomp
 
 puts "Please choose price_from: "
-$hashWithMyImputs[:price_from] = gets.chomp
+$hash_with_my_imputs[:price_from] = gets.chomp
 
 puts "Please choose price_to: "
-$hashWithMyImputs[:price_to] = gets.chomp
+$hash_with_my_imputs[:price_to] = gets.chomp
 
 
 # load a yaml file + symbolize all keys of hashes
-$carsFile = YAML.safe_load_file('cars.yml', symbolize_names: true) 
+$cars_file = YAML.safe_load_file('cars.yml', symbolize_names: true) 
 
 #create a new array to save all cars from yaml 
-$arrWithCars = []
+$arr_with_cars = []
 
-def addAllCarsFromYALM
-$carsFile.map do |car|
-        $arrWithCars << car
+def add_all_cars_from_YALM
+$cars_file.map do |car|
+        $arr_with_cars << car
     end
 end
 
 #call the function and add all our machines to the array
-addAllCarsFromYALM
+add_all_cars_from_YALM
 
-#$arrWithCars
+#$arr_with_cars
 
 
-def filterArrWithCarsByMakeAndModel(rule)
-    skipByEmptyValue = $hashWithMyImputs[rule] == ""
-    $arrWithCars.keep_if {|car| car[rule] == $hashWithMyImputs[rule] || skipByEmptyValue}
+def filter_arr_with_cars_by_make_and_model(rule)
+    skip_by_empty_value = $hash_with_my_imputs[rule] == ""
+    $arr_with_cars.keep_if {|car| car[rule] == $hash_with_my_imputs[rule] || skip_by_empty_value}
 end
 
 
 make = :make
 model = :model
 
-filterArrWithCarsByMakeAndModel(make)
-filterArrWithCarsByMakeAndModel(model)
+filter_arr_with_cars_by_make_and_model(make)
+filter_arr_with_cars_by_make_and_model(model)
 
-#puts $arrWithCars
+#puts $arr_with_cars
 
 
-def filterArrWithCarsByYearAndPrice(rule, parameterFrom, parameterTo)
+def filter_arr_with_cars_by_year_and_price(rule, parameter_from, parameter_to)
 
-    from = $hashWithMyImputs[parameterFrom]
-    tok = $hashWithMyImputs[parameterTo]
+    from = $hash_with_my_imputs[parameter_from]
+    tok = $hash_with_my_imputs[parameter_to]
 
-    skipByEmptyFrom = from == ""
-    skipByEmptyTo = tok == ""
+    skip_by_empty_from = from == ""
+    skip_by_empty_to = tok == ""
 
-    $arrWithCars.keep_if do |car|
-        (car[rule] >= from.to_i || skipByEmptyFrom) && (car[rule] <= tok.to_i || skipByEmptyTo)
+    $arr_with_cars.keep_if do |car|
+        (car[rule] >= from.to_i || skip_by_empty_from) && (car[rule] <= tok.to_i || skip_by_empty_to)
     end
 end
 
@@ -89,7 +89,7 @@ year_to = :year_to
 
 #call a function that filter the array of cars according to the required parameters
 #filter by given date of manufacture of the car
-filterArrWithCarsByYearAndPrice(year, year_from, year_to)
+filter_arr_with_cars_by_year_and_price(year, year_from, year_to)
 
 #put the keys in separate variables
 price = :price
@@ -98,25 +98,25 @@ price_to = :price_to
 
 #call a function that filter the array of cars according to the required parameters
 #filter by the given price range
-filterArrWithCarsByYearAndPrice(price, price_from, price_to)
+filter_arr_with_cars_by_year_and_price(price, price_from, price_to)
 
 
 # defolt sort by date_added. Sort by decline
-$arrWithCars = $arrWithCars.sort_by {|car| Date.strptime(car[:date_added], '%d/%m/%y')}.reverse 
+$arr_with_cars = $arr_with_cars.sort_by {|car| Date.strptime(car[:date_added], '%d/%m/%y')}.reverse 
 
 
 # get order parameter and diraction for sort process
 puts ("Please choose sort option (date_added|price): ")
-orderBy = gets.chomp
+order_by = gets.chomp
 
 puts ("Please choose sort direction(desc|asc): ")
-orderDirection = gets.chomp
+order_direction = gets.chomp
 
 
 # condition for sort by parametere and diraction
-orderBy == "price"  ?  $arrWithCars.sort_by! {|car| car[:price]} :  $arrWithCars.sort_by! {|car| Date.strptime(car[:date_added], '%d/%m/%y')}
+order_by == "price"  ?  $arr_with_cars.sort_by! {|car| car[:price]} :  $arr_with_cars.sort_by! {|car| Date.strptime(car[:date_added], '%d/%m/%y')}
 
-orderDirection == "asc" ? $arrWithCars : $arrWithCars.reverse!
+order_direction == "asc" ? $arr_with_cars : $arr_with_cars.reverse!
 
 #call function to show the result of executing the request according to the specified parameters
-showRequestedCar
+show_requested_car 
