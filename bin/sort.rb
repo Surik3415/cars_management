5f904168e3b60e5e12ba3b92a1142e7c5b70909e
SurenAvakian <Surenavakian96@gmail.com>
2022-11-24 01:08:27 +0200

    change sort method becose of rubocop

--- bin/sort.rb
+++ bin/sort.rb
@@ -17,9 +17,7 @@ class Sort
         car['price']
       end
     else
-      @result_of_search.sort_by! do |car|
-        Date.strptime(car['date_added'], '%d/%m/%y')
-      end
+      @result_of_search.sort_by! { |car| Date.strptime(car['date_added'], '%d/%m/%y') }
     end
     @rules['order_direction'] == 'asc' ? @result_of_search : @result_of_search.reverse!
   end
