54278df3d2e2ec71e8dc9b13feb3c58f173d2985
SurenAvakian <Surenavakian96@gmail.com>
2022-12-06 20:30:41 +0200

    detate duplicatet file cars.yml

--- index.rb
+++ index.rb
@@ -1,12 +1,5 @@
-
 # frozen_string_literal: true
 
 require_relative 'bin/dependencies'
 
 RunApp.new.call
-
-
-#call function to show the result of executing the request according to the specified parameters
-show_requested_car(cars_file) 
-
-
